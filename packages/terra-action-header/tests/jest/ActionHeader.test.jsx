9d810ea7c536c7076a835719383d49ecc910c4dc
Chase Sinclair <chasersinclair@gmail.com>
2018-03-21 10:24:05 -0500

    Update jest tests with translations

--- packages/terra-action-header/tests/jest/ActionHeader.test.jsx
+++ packages/terra-action-header/tests/jest/ActionHeader.test.jsx
@@ -1,35 +1,84 @@
+/* eslint-disable import/no-extraneous-dependencies, no-alert */
 import React from 'react';
+import { IntlProvider } from 'react-intl';
+import Button from 'terra-button';
 import ActionHeader from '../../src/ActionHeader';
+import messages from '../../translations/en-US.json';
+
+const locale = 'en-US';
 
 describe('ActionHeader', () => {
   // Snapshot Tests
   it('should render a default action header', () => {
-    const actionHeader = render(<ActionHeader />);
+    const actionHeader = shallow(
+      <IntlProvider locale={locale} messages={messages}>
+        <ActionHeader />
+      </IntlProvider>,
+    );
     expect(actionHeader).toMatchSnapshot();
   });
 
   it('should render an action header with title', () => {
-    const actionHeader = render(<ActionHeader title="Action Header" />);
+    const actionHeader = shallow(
+      <IntlProvider locale={locale} messages={messages}>
+        <ActionHeader title="Action Header" />
+      </IntlProvider>,
+    );
     expect(actionHeader).toMatchSnapshot();
   });
 
   it('should render an action header with back button and title', () => {
-    const actionHeader = render(<ActionHeader onBack={() => {}} title="Action Header" />);
+    const actionHeader = shallow(
+      <IntlProvider locale={locale} messages={messages}>
+        <ActionHeader title="Action Header" onBack={() => {}} />
+      </IntlProvider>,
+    );
     expect(actionHeader).toMatchSnapshot();
   });
 
   it('should render an action header with close button and title', () => {
-    const actionHeader = render(<ActionHeader onClose={() => {}} title="Action Header" />);
+    const actionHeader = shallow(
+      <IntlProvider locale={locale} messages={messages}>
+        <ActionHeader title="Action Header" onClose={() => {}} />
+      </IntlProvider>,
+    );
     expect(actionHeader).toMatchSnapshot();
   });
 
   it('should render an action header with back and close buttons and title', () => {
-    const actionHeader = render(<ActionHeader onBack={() => {}} onClose={() => {}} title="Action Header" />);
+    const actionHeader = shallow(
+      <IntlProvider locale={locale} messages={messages}>
+        <ActionHeader title="Action Header" onBack={() => {}} onClose={() => {}} />
+      </IntlProvider>,
+    );
     expect(actionHeader).toMatchSnapshot();
   });
 
   it('should render an action header with level three header element and title', () => {
-    const actionHeader = render(<ActionHeader level={3} title="Action Header" />);
+    const actionHeader = shallow(
+      <IntlProvider locale={locale} messages={messages}>
+        <ActionHeader title="Action Header" level={3} />
+      </IntlProvider>,
+    );
     expect(actionHeader).toMatchSnapshot();
   });
+
+  it('should render an action header with custom button and title', () => {
+    const actionHeader = shallow(
+      <IntlProvider locale={locale} messages={messages}>
+        <ActionHeader title="Action Header" >
+          <Button text="Custom Button" onClick={() => alert('You clicked me!')} />
+        </ActionHeader>
+      </IntlProvider>,
+    );
+    expect(actionHeader).toMatchSnapshot();
+  });
+});
+
+it('throws error on missing locale prop in Base', () => {
+  try {
+    render(<ActionHeader />);
+  } catch (e) {
+    expect(e.message).toContain('add locale prop to Base component');
+  }
 });
